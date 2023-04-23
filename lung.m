6841f015652547740dae899cefc138ce992ce4d0
sskalak <sskalak@ucsd.edu>
2023-04-23 14:06:59 -0700

    task 6
    task 6
    scott skalak

--- lung.m
+++ lung.m
@@ -9,7 +9,7 @@ global Pstar cstar n maxcount M Q camax RT cI;
 %vs perfursion for the tasks now to better compare someone used to sealevel
 %or altitude without the randomness affecting my numbers
 
-%using percent O2 at various altitudes to then do task 6 next easily 
+%using percent O2 at various altitudes 
 O2percents = [20.9 20.1 19.4 18.6 17.9 17.3 16.6 16.0 15.4 14.8 14.3 13.7 13.2 12.7 12.3 11.8 11.4 11.0 10.5 10.1 9.7 9.4 9.0 8.7 8.4 8.1 7.8 7.5 7.2 6.9]/100;
 altitude = 0:1000:29000;
 
@@ -36,28 +36,26 @@ cAbarlist(i) = cAbar;
 cIlist(i) = cI
 end
 
-%plotting cI vs mean partial pressures
+%plotting altitude vs mean partial pressures
 figure('NumberTitle', 'off', 'Name', 'altitude vs O2 Pressure');
-plot(cIlist, Palist);
+plot(altitude, Palist);
 hold on
-plot(cIlist, PAlist);
-plot(cIlist, Pvlist); 
+plot(altitude, PAlist);
+plot(altitude, Pvlist); 
 legend('mean arterial O2 partial pressure','mean alveolar O2 partial pressure','O2 partial pressure in venous blood')
-xlabel('cI (moles/L')
- set(gca, 'XTickLabel',get(gca,'XTick')) 
- set ( gca, 'xdir', 'reverse' )
+xlabel('altitude (feet)')
+set(gca, 'XTickLabel',get(gca,'XTick')) 
 ylabel('Partial pressures of O2 (mmHg)')
-title('cI vs partial pressure of O2')
+title('altitude vs partial pressure of O2')
 
-%plotting cI vs mean o2 concentrations
+%plotting altitude vs mean o2 concentrations
 figure('NumberTitle', 'off', 'Name', 'altitude vs O2 Conc');
-plot(cIlist, cvlist);
+plot(altitude, cvlist);
 hold on
-plot(cIlist, cabarList);
-plot(cIlist, cAbarlist); 
+plot(altitude, cabarList);
+plot(altitude, cAbarlist); 
 legend('O2 conc in venous blood','mean arterial O2 conc','mean alveolar O2 conc','Location', 'southwest')
-xlabel('cI (moles/L)')
+xlabel('altitude (feet)')
 ylabel('Concentrations of O2 (moles/Liter)')
- set(gca, 'XTickLabel',get(gca,'XTick')) 
- set ( gca, 'xdir', 'reverse' )
-title('cI vs conc of O2')
+set(gca, 'XTickLabel',get(gca,'XTick')) 
+title('altitude vs conc of O2')
